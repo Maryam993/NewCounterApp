2d2b4696c085e6518ddb0495d684e85005cbae62
Maryam993 <96576205+Maryam993@users.noreply.github.com>
2022-01-04 12:27:33 +1100

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react'
+import React from 'react'
+import { Text, View, Button } from 'react-native'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+  const [counter, setCounter] = useState(0)
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const increment = () => {
+    setCounter(counter + 1)
+  }
+  const decrement = () => {
+    setCounter(counter - 1)
+  }
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        marginBottom: 6,
+        justifyContent: 'center',
+        backgroundColor: 'pink',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Counter App</Text>
+      <Text testID="myValue">{counter}</Text>
+
+      <Button
+        testID="myButton"
+        title="Increment"
+        onPress={() => increment()}
+        disabled={counter === 10}
+      />
+
+      <Button
+        testID="myButton2"
+        title="decrement"
+        onPress={() => decrement()}
+        disabled={counter === 0}
+      />
+    </View>
+  )
+}
